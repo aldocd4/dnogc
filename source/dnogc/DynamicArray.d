717e561dfb1470f0cdb8befa54f08627f3bb4cfd
Robert G <Robert G>
2017-10-23 20:58:34 +0200

    - Space to Tab for indent

--- source/dnogc/DynamicArray.d
+++ source/dnogc/DynamicArray.d
@@ -7,8 +7,8 @@ import dnogc.Utils;
 
 debug
 {
-    public static size_t totalBytesAllocated;
-    public static size_t totalBytesFreed;	
+	public static size_t totalBytesAllocated;
+	public static size_t totalBytesFreed;
 }
 
 /**
@@ -17,20 +17,20 @@ debug
  */
 struct DynamicArray(T, bool useGC = hasIndirections!T)
 {
-    private size_t m_length;
-    private size_t m_capacity;
+	private size_t m_length;
+	private size_t m_capacity;
 
-    private T[] m_array;
+	private T[] m_array;
 
-    alias opDollar = length;
+	alias opDollar = length;
 
-    public this(in int capacity, string file = __FILE__, int line = __LINE__) @trusted nothrow @nogc
-    {
-        this.m_capacity = capacity;
-        
-        if(capacity != 0)
-        {
-            this.m_array = cast(T[])Mallocator.instance.allocate(T.sizeof * capacity);
+	public this(in int capacity, string file = __FILE__, int line = __LINE__) @trusted nothrow @nogc
+	{
+		this.m_capacity = capacity;
+
+		if(capacity != 0)
+		{
+			this.m_array = cast(T[])Mallocator.instance.allocate(T.sizeof * capacity);
 
 			static if(useGC)
 			{
@@ -38,32 +38,32 @@ struct DynamicArray(T, bool useGC = hasIndirections!T)
 				GC.addRange(this.m_array.ptr, T.sizeof * capacity);
 			}
 
-            debug
-            {
-                totalBytesAllocated += T.sizeof * capacity;
-                // dln("Allocated " , T.sizeof * capacity , " bytes from ", file, " at ", line);
-            }
-        }
-    }
-    
-    public void dispose()
-    {
-        if(this.m_array is null)
-        {
-            return;
-        }
-    
-        foreach(ref element; this.m_array[0 .. this.m_length])
-        {
-            static if(is(T == class) || is(T == interface))
-            {
-                destroy(element);
-            }
-            else
-            {
-                typeid(T).destroy(&element);
-            }
-        }
+			debug
+			{
+				totalBytesAllocated += T.sizeof * capacity;
+				// dln("Allocated " , T.sizeof * capacity , " bytes from ", file, " at ", line);
+			}
+		}
+	}
+
+	public void dispose()
+	{
+		if(this.m_array is null)
+		{
+			return;
+		}
+
+		foreach(ref element; this.m_array[0 .. this.m_length])
+		{
+			static if(is(T == class) || is(T == interface))
+			{
+				destroy(element);
+			}
+			else
+			{
+				typeid(T).destroy(&element);
+			}
+		}
 
 		static if(useGC)
 		{
@@ -71,42 +71,42 @@ struct DynamicArray(T, bool useGC = hasIndirections!T)
 			GC.removeRange(this.m_array.ptr);
 		}
 
-        if(this.m_capacity != 0)
-        {			
-            Mallocator.instance.deallocate(this.m_array);
+		if(this.m_capacity != 0)
+		{
+			Mallocator.instance.deallocate(this.m_array);
 
-            debug 
-            {
-                totalBytesFreed += T.sizeof * m_capacity;
-                // dln("Freed " , T.sizeof * this.m_capacity , " bytes");
-            }
-        }
-    }
+			debug
+			{
+				totalBytesFreed += T.sizeof * m_capacity;
+				// dln("Freed " , T.sizeof * this.m_capacity , " bytes");
+			}
+		}
+	}
 
-    public typeof(this) copy() @trusted nothrow @nogc
-    {
-        auto ret = DynamicArray!T(this.m_capacity);
-        ret.m_length = this.m_length;
+	public typeof(this) copy() @trusted nothrow @nogc
+	{
+		auto ret = DynamicArray!T(this.m_capacity);
+		ret.m_length = this.m_length;
 
-        if(this.m_capacity != 0)
-        {
-            import core.stdc.string;
+		if(this.m_capacity != 0)
+		{
+			import core.stdc.string;
 
-            memcpy(ret.m_array.ptr, this.m_array.ptr, T.sizeof * this.m_capacity);			
-        }
+			memcpy(ret.m_array.ptr, this.m_array.ptr, T.sizeof * this.m_capacity);
+		}
 
-        return ret;
-    }
+		return ret;
+	}
 
-    /**
-     * Inserts element at the end of array
-     */
-    public void insert(T element, string file = __FILE__, int line = __LINE__) nothrow @trusted @nogc
-    {
-        if(this.m_array is null || this.m_array.length == 0)
-        {
-            this.m_capacity = 4;
-            this.m_array = cast(T[]) Mallocator.instance.allocate(T.sizeof * this.m_capacity);
+	/**
+	 * Inserts element at the end of array
+	 */
+	public void insert(T element, string file = __FILE__, int line = __LINE__) nothrow @trusted @nogc
+	{
+		if(this.m_array is null || this.m_array.length == 0)
+		{
+			this.m_capacity = 4;
+			this.m_array = cast(T[]) Mallocator.instance.allocate(T.sizeof * this.m_capacity);
 
 			static if(useGC)
 			{
@@ -114,209 +114,209 @@ struct DynamicArray(T, bool useGC = hasIndirections!T)
 				GC.addRange(this.m_array.ptr, T.sizeof * this.m_capacity);
 			}
 
-            debug totalBytesAllocated += T.sizeof * m_capacity;
-            // debug dln("Allocated " , T.sizeof * this.m_capacity, " bytes from ", file, " at ", line);
-        }
-        else if(this.m_length >= this.m_array.length)
-        {
-            debug
-            {
-                auto alreadyAllocated = T.sizeof * m_capacity;
-            }
+			debug totalBytesAllocated += T.sizeof * m_capacity;
+			// debug dln("Allocated " , T.sizeof * this.m_capacity, " bytes from ", file, " at ", line);
+		}
+		else if(this.m_length >= this.m_array.length)
+		{
+			debug
+			{
+				auto alreadyAllocated = T.sizeof * m_capacity;
+			}
 
-            this.m_capacity = this.m_array.length > 512 ? this.m_array.length + 1024 : this.m_array.length << 1;
+			this.m_capacity = this.m_array.length > 512 ? this.m_array.length + 1024 : this.m_array.length << 1;
 
-            auto ptr = cast(void[])this.m_array;
+			auto ptr = cast(void[])this.m_array;
 
-            debug totalBytesAllocated += (T.sizeof * m_capacity) - alreadyAllocated;
+			debug totalBytesAllocated += (T.sizeof * m_capacity) - alreadyAllocated;
 
-            Mallocator.instance.reallocate(ptr, T.sizeof * this.m_capacity);
+			Mallocator.instance.reallocate(ptr, T.sizeof * this.m_capacity);
 
-            // debug dln("Reallocated " , T.sizeof * this.m_capacity, " bytes from ", file, " at ", line);
+			// debug dln("Reallocated " , T.sizeof * this.m_capacity, " bytes from ", file, " at ", line);
 
-            this.m_array = cast(T[])ptr;
+			this.m_array = cast(T[])ptr;
 
-            static if(useGC)
+			static if(useGC)
 			{
 				import core.memory: GC;
 				GC.removeRange(this.m_array.ptr);
 				GC.addRange(this.m_array.ptr, T.sizeof * this.m_capacity);
 			}
-        }
+		}
 
-        this.m_array[this.m_length++] = element;
-    }
+		this.m_array[this.m_length++] = element;
+	}
 
-    
-    /**
-     * Slice operator overload
-     */
-    pragma(inline, true)
-    public auto opSlice(this This)() nothrow @safe @nogc
-    {
-        return opSlice!(This)(0, this.m_length);
-    }
 
-    pragma(inline, true)
-    public auto opSlice(this This)(size_t a, size_t b) nothrow @safe @nogc
-    {
-        return this.m_array[a .. b];
-    }
+	/**
+	 * Slice operator overload
+	 */
+	pragma(inline, true)
+	public auto opSlice(this This)() nothrow @safe @nogc
+	{
+		return opSlice!(This)(0, this.m_length);
+	}
 
-    /**
-     * Index operator overload
-     */
-    pragma(inline, true)
-    public auto opIndex(this This)(size_t i) nothrow @safe @nogc
-    {
-        return this.m_array[i];
-    }
+	pragma(inline, true)
+	public auto opSlice(this This)(size_t a, size_t b) nothrow @safe @nogc
+	{
+		return this.m_array[a .. b];
+	}
 
-    /**
-     * ~= operator overload
-     */
-    nothrow @safe @nogc
-    public void opOpAssign(string op)(T value) if (op == "~")
-    {
-        this.insert(value);
-    }
+	/**
+	 * Index operator overload
+	 */
+	pragma(inline, true)
+	public auto opIndex(this This)(size_t i) nothrow @safe @nogc
+	{
+		return this.m_array[i];
+	}
 
-    /**
-     * ~ operator overload
-     */
-    nothrow @safe @nogc
-    public typeof(this) opBinary(string op)(ref typeof(this) other) if (op == "~")
-    {
-        typeof(this) ret;
+	/**
+	 * ~= operator overload
+	 */
+	nothrow @safe @nogc
+	public void opOpAssign(string op)(T value) if (op == "~")
+	{
+		this.insert(value);
+	}
 
-        foreach(element; this.m_array[0 .. this.m_length])
-        {
-            ret.insert(element);
-        }
+	/**
+	 * ~ operator overload
+	 */
+	nothrow @safe @nogc
+	public typeof(this) opBinary(string op)(ref typeof(this) other) if (op == "~")
+	{
+		typeof(this) ret;
 
-        foreach(element; other.m_array[0 .. other.m_length])
-        {
-            ret.insert(element);
-        }
+		foreach(element; this.m_array[0 .. this.m_length])
+		{
+			ret.insert(element);
+		}
 
-        return ret;
-    }
+		foreach(element; other.m_array[0 .. other.m_length])
+		{
+			ret.insert(element);
+		}
 
-    nothrow @safe @nogc
-    public typeof(this) opBinary(string op)(T[] elements) if (op == "~")
-    {
-        typeof(this) ret;
+		return ret;
+	}
 
-        foreach(element; this.m_array[0 .. this.m_length])
-        {
-            ret.insert(element);
-        }
+	nothrow @safe @nogc
+	public typeof(this) opBinary(string op)(T[] elements) if (op == "~")
+	{
+		typeof(this) ret;
 
-        foreach(element; elements)
-        {
-            ret.insert(element);
-        }
+		foreach(element; this.m_array[0 .. this.m_length])
+		{
+			ret.insert(element);
+		}
 
-        return ret;
-    }
+		foreach(element; elements)
+		{
+			ret.insert(element);
+		}
 
-    /**
-     * Removes the item at the given index from the array
-     */
-    public void remove(in size_t i)
-    {
-        if(i < this.m_length)
-        {
-            static if (is(T == class) || is(T == interface))
-            {
-                destroy(this.m_array[i]);
-            }
-            else
-            {
-                typeid(T).destroy(&this.m_array[i]);
-            }
+		return ret;
+	}
 
-            auto next = i + 1;
+	/**
+	 * Removes the item at the given index from the array
+	 */
+	public void remove(in size_t i)
+	{
+		if(i < this.m_length)
+		{
+			static if (is(T == class) || is(T == interface))
+			{
+				destroy(this.m_array[i]);
+			}
+			else
+			{
+				typeid(T).destroy(&this.m_array[i]);
+			}
 
-            while(next < this.m_length)
-            {
-                this.m_array[next - 1] = this.m_array[next];
-                ++next;
-            }
+			auto next = i + 1;
 
-            this.m_length -= 1;
-        }
-        else
-        {
-            import core.exception : RangeError;
-            throw new RangeError("Out of range index used to remove element");
-        }
-    }
+			while(next < this.m_length)
+			{
+				this.m_array[next - 1] = this.m_array[next];
+				++next;
+			}
 
-    /**
-     * Removes the last element from the array
-     */
-    public void removeBack()
-    {
-        this.remove(this.m_length - 1);
-    }
+			this.m_length -= 1;
+		}
+		else
+		{
+			import core.exception : RangeError;
+			throw new RangeError("Out of range index used to remove element");
+		}
+	}
 
-    /**
-     * Index assignment support
-     */
-    public void opIndexAssign(T value, size_t i) nothrow @safe @nogc
-    {
-        this.m_array[i] = value;
-    }
+	/**
+	 * Removes the last element from the array
+	 */
+	public void removeBack()
+	{
+		this.remove(this.m_length - 1);
+	}
 
-    /**
-     * Slice assignment support
-     */
-    public void opSliceAssign(T value) nothrow @safe @nogc
-    {
-        this.m_array[0 .. this.m_length] = value;
-    }
+	/**
+	 * Index assignment support
+	 */
+	public void opIndexAssign(T value, size_t i) nothrow @safe @nogc
+	{
+		this.m_array[i] = value;
+	}
 
-    public void opSliceAssign(T value, size_t i, size_t j) nothrow @safe @nogc
-    {
-        this.m_array[i .. j] = value;
-    }
+	/**
+	 * Slice assignment support
+	 */
+	public void opSliceAssign(T value) nothrow @safe @nogc
+	{
+		this.m_array[0 .. this.m_length] = value;
+	}
 
-    @property
-    {
-        public size_t length() const pure nothrow @safe @nogc 
-        {
-            return this.m_length; 
-        }
+	public void opSliceAssign(T value, size_t i, size_t j) nothrow @safe @nogc
+	{
+		this.m_array[i .. j] = value;
+	}
 
-        /**
-         * Use this property only if you use functions like fread
-         */
-        public void length(in size_t value) pure nothrow @safe @nogc
-        {
-            assert(value <= this.m_capacity);
-            this.m_length = value;
-        }
+	@property
+	{
+		public size_t length() const pure nothrow @safe @nogc
+		{
+			return this.m_length;
+		}
 
-        public bool empty() const pure nothrow @safe @nogc 
-        { 
-            return this.m_length == 0; 
-        }
+		/**
+		 * Use this property only if you use functions like fread
+		 */
+		public void length(in size_t value) pure nothrow @safe @nogc
+		{
+			assert(value <= this.m_capacity);
+			this.m_length = value;
+		}
 
-        public void* ptr(this This)() nothrow @safe @nogc
-        {
-            return &this.m_array[0];
-        }
+		public bool empty() const pure nothrow @safe @nogc
+		{
+			return this.m_length == 0;
+		}
 
-        public auto ref T front() nothrow @safe @nogc
-        {
-            return this.m_array[0];
-        }
+		public void* ptr(this This)() nothrow @safe @nogc
+		{
+			return &this.m_array[0];
+		}
 
-        public auto ref T back() nothrow @safe @nogc
-        {
-            return this.m_array[this.m_length - 1];
-        }
-    }
+		public auto ref T front() nothrow @safe @nogc
+		{
+			return this.m_array[0];
+		}
+
+		public auto ref T back() nothrow @safe @nogc
+		{
+			return this.m_array[this.m_length - 1];
+		}
+	}
 }
 
--- source/dnogc/Utils.d
+++ source/dnogc/Utils.d
@@ -2,31 +2,31 @@ module dnogc.Utils;
 
 T nogcNew(T, Args...) (Args args) @trusted @nogc
 {
-    import std.conv : emplace;
-    import core.stdc.stdlib : malloc;
+	import std.conv : emplace;
+	import core.stdc.stdlib : malloc;
 
-    auto size = __traits(classInstanceSize, T);
+	auto size = __traits(classInstanceSize, T);
 
-    auto memory = malloc(size)[0..size];
-    if(!memory)
-    {
-        import core.exception : onOutOfMemoryError;
-        onOutOfMemoryError();
-    }
+	auto memory = malloc(size)[0..size];
+	if(!memory)
+	{
+		import core.exception : onOutOfMemoryError;
+		onOutOfMemoryError();
+	}
 
-    // call T's constructor and emplace instance on
-    // newly allocated memory
-    return emplace!(T, Args)(memory, args);
+	// call T's constructor and emplace instance on
+	// newly allocated memory
+	return emplace!(T, Args)(memory, args);
 }
 
 void nogcDel(T)(T obj) @trusted
 {
-    import core.stdc.stdlib : free;
+	import core.stdc.stdlib : free;
 
-    destroy(obj);
+	destroy(obj);
 
-    // free memory occupied by object
-    free(cast(void*)obj);
+	// free memory occupied by object
+	free(cast(void*)obj);
 }
 
 /**
@@ -34,28 +34,28 @@ void nogcDel(T)(T obj) @trusted
  */
 void assumeNogc(alias Func, T...)(T xs) @nogc
 {
-    import std.traits : isFunctionPointer, isDelegate, functionAttributes,
-        FunctionAttribute, SetFunctionAttributes, functionLinkage;
+	import std.traits : isFunctionPointer, isDelegate, functionAttributes,
+		FunctionAttribute, SetFunctionAttributes, functionLinkage;
 
-    static auto assumeNogcPtr(T)(T f) if (isFunctionPointer!T || isDelegate!T)
-    {
-        enum attrs = functionAttributes!T | FunctionAttribute.nogc;
-        return cast(SetFunctionAttributes!(T, functionLinkage!T, attrs)) f;
-    }
-    
-    assumeNogcPtr(&Func!T)(xs);
+	static auto assumeNogcPtr(T)(T f) if (isFunctionPointer!T || isDelegate!T)
+	{
+		enum attrs = functionAttributes!T | FunctionAttribute.nogc;
+		return cast(SetFunctionAttributes!(T, functionLinkage!T, attrs)) f;
+	}
+
+	assumeNogcPtr(&Func!T)(xs);
 }
 
 void dln(string file = __FILE__, uint line = __LINE__, string fun = __FUNCTION__, Args...)(Args args) pure nothrow @trusted
 {
-    try
-    {
-        import std.stdio : writeln;
+	try
+	{
+		import std.stdio : writeln;
 
-        debug assumeNogc!writeln(file, ":", line, ":", " debug: ", args);
-    }
-    catch (Exception)
-    {
-    }
+		debug assumeNogc!writeln(file, ":", line, ":", " debug: ", args);
+	}
+	catch (Exception)
+	{
+	}
 }
 
